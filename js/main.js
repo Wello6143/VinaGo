606d218ec3b7f38e53512cb9661b5a8ac40f443c
Wello <wello6143@outlook.com.vn>
2018-06-26 17:36:12 +0700

    Commit nhẹ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-$(function() {
-	$( "#nav-tabs-tab" ).draggable();
-});
+console.log("%cDỪNG LẠI!!!","color: red; font-size: 25px;");
+console.log("%cNếu ai đó nói với bạn vào đây để hack tài khoản bằng cách sao chép rồi dán ở đây thì chắc chắn 200% bạn bị lừa một cách ngoạn mục.","font-size: 15px;");
+console.log("%cNgược lại, bạn sẽ bị hack ngược lại tài khoản của bạn, mình xin chắc chắn là vậy.","font-size: 15px;");
+console.log("%cVì vậy, nếu bạn không biết chỗ này làm gì, hãy đóng và thoát ra ngay tức khắc để được an toàn. Còn nếu bạn là lập trình viên và biết làm gì, thì chúc vui vẻ ;).","font-size: 15px;")
